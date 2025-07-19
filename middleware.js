4999abf6612520688c425bf9688c202424a02989
deathtroopers2000 <123949630+deathtroopers2000@users.noreply.github.com>
2025-07-19 22:59:58 +0330

    Update middleware.js

--- middleware.js
+++ middleware.js
@@ -4,10 +4,13 @@ export const config = {
 
 export default async function middleware(request) {
   const url = new URL(request.url);
- const destination = 'https://51.178.149.237';
+  const destination = 'https://51.178.149.237';
+
+  const headers = new Headers(request.headers);
+  headers.set('Host', '51.178.149.237'); // یا دامنه واقعی سرور اگر لازم باشه
 
   return fetch(destination + url.pathname + url.search, {
-    headers: request.headers,
+    headers,
     method: request.method,
     body: request.body,
   });
